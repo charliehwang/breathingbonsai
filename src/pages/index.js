7506722be96b13317c15fa8e55e4f8377f87e4e0
charlie <charliehwang@gmail.com>
2021-05-30 12:56:13 -0600

    Ignoring public directory

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,16 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+// import { Link } from "gatsby"
+// import { StaticImage } from "gatsby-plugin-image"
+import Portfolio from "../components/portfolio"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <Seo title="Home" />
+    <Seo title="Breathing Bonsai" />
     <h1>Breathing Bonsai</h1>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
+    <Portfolio />
   </Layout>
 )
 
